5e00f97d8a8d6dfb88888d15e4e3f548442fa77e
snehav <snehavreddy12@gmail.com>
2026-08-15 11:47:26 +0530

    Add JWT configuration

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    PROJECT_NAME = "Car Price API"
+
+    API_KEY = os.getenv("API_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM = "HS256"
+
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+    MODEL_PATH = "app/models/model.joblib"
+
+
+settings = Settings()
